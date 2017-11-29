16dd979d852e7f45abaa993b40fa4ae95666c050
bruseghini92 <bruseghini92.ab@gmail.com>
2017-11-29 09:00:09 -0300

    Seguimiento

--- src/main/java/ar/edu/um/ingenieria/controller/apirest/SeguimientoController.java
+++ src/main/java/ar/edu/um/ingenieria/controller/apirest/SeguimientoController.java
@@ -2,47 +2,58 @@ package ar.edu.um.ingenieria.controller.apirest;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import ar.edu.um.ingenieria.domain.Estado;
-import ar.edu.um.ingenieria.domain.Etapa;
-import ar.edu.um.ingenieria.domain.Planta;
 import ar.edu.um.ingenieria.domain.Seguimiento;
-import ar.edu.um.ingenieria.domain.Tarea;
-import ar.edu.um.ingenieria.domain.Usuario;
+import ar.edu.um.ingenieria.service.impl.SeguimientoServiceImpl;
 
 @RestController
-@RequestMapping("/seguimiento")
+@RequestMapping(value = "/seguimiento" , method = RequestMethod.GET)
 public class SeguimientoController {
-	/*
 	@Autowired
-	private SeguimientoManager seguimientoManager;
+	private SeguimientoServiceImpl seguimientoService;
 	
-	public ResponseEntity<List<Seguimiento>> findAll() {
-		return new ResponseEntity<List<Seguimiento>>(seguimientoManager.findAll(), HttpStatus.OK);
+
+private static final Logger logger = LoggerFactory.getLogger(SeguimientoController.class);
+	
+
+	@GetMapping
+    public List<Seguimiento> indexPage() {
+		logger.info("datos de seguimiento: {}", seguimientoService.findAll());
+	    return seguimientoService.findAll();
+    }
+	
+	@GetMapping("/{id}")
+    public Seguimiento show(@PathVariable Integer id) {
+		logger.info("datos de seguimiento: {}", seguimientoService.findAll());
+	    return seguimientoService.findById(id);
+    }
+	
+	@PostMapping
+    public Seguimiento add(@RequestBody Seguimiento seguimiento) {
+	    return seguimientoService.create(seguimiento);
+    }
+	
+	@PutMapping(value = "/{id}")
+    public Seguimiento update(@RequestBody Seguimiento seguimiento, @PathVariable Integer id) {
+		seguimiento.setId(id);
+	    return seguimientoService.update(seguimiento);
+    }
+	
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable Integer id) {
+		seguimientoService.remove(seguimientoService.findById(id));
 	}
-	
-	@GetMapping("/seguimiento/nuevo")
-	public ResponseEntity<Seguimiento> edit(@PathVariable Usuario usuario, Planta planta, Estado estado, Tarea tarea, Etapa etapa, Seguimiento seguimiento) {
-		seguimientoManager.create(usuario, planta, estado, tarea, etapa, seguimiento);
-		return new ResponseEntity<Seguimiento>(seguimientoManager.findById(seguimiento.getId()),HttpStatus.OK);
-	}
-	
-	@GetMapping("/seguimiento/{id}")
-	public ResponseEntity<Seguimiento> edit(@PathVariable Integer id) {
-		return new ResponseEntity<Seguimiento>(seguimientoManager.findById(id),HttpStatus.OK);
-	}
-	
-	@DeleteMapping("/seguimiento/{id}")
-	public ResponseEntity<Void>  delete(@PathVariable Integer id) {
-		seguimientoManager.delete(id);
-		return new ResponseEntity<Void>(HttpStatus.OK);
-	}*/
 }
