eab1f596f1949df8796479e93fa7bd550e5c435e
leansabio <leansabio@gmail.com>
2018-02-26 12:49:23 +0000

    creado ventas manger y añadido borrar ventas a venta controler

--- src/main/java/ar/edu/um/ingenieria/controller/ventas/VentasController.java
+++ src/main/java/ar/edu/um/ingenieria/controller/ventas/VentasController.java
@@ -1,36 +1,56 @@
 package ar.edu.um.ingenieria.controller.ventas;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.edu.um.ingenieria.domain.Venta;
-//import ar.edu.um.ingenieria.manager.VentaManager;
+import ar.edu.um.ingenieria.manager.VentaManager;
 import ar.edu.um.ingenieria.service.impl.VentaServiceImpl;
 
 @RestController
 @RequestMapping("/ventas")
 public class VentasController {  // no funciona nada
-	//@Autowired
-	//private VentaManager ventaManager;
 	@Autowired
+	private VentaManager ventaManager;
 	private VentaServiceImpl ventaServiceImpl;
+	
+	@GetMapping("/ventas/")
+	public ResponseEntity<List<Venta>> findAll() {
+		return new ResponseEntity<List<Venta>>(ventaServiceImpl.findAll(), HttpStatus.OK);
+	}
+
+	@GetMapping("/ventas/{id}")
+	public ResponseEntity<Venta> edit(@PathVariable Integer id) {
+		return new ResponseEntity<Venta>(ventaServiceImpl.findById(id),HttpStatus.OK);
+	}
 	
 	@PostMapping("/crear")
 	public ResponseEntity<Void> agregar(Venta venta) {
-		System.out.println("daasdasdas "+venta);
 		ventaServiceImpl.create(venta);
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
 	@PostMapping("/actulizar")
 	public ResponseEntity<Void> actulizar(Venta venta) {
-		System.out.println("daasdasdas "+venta);
 		ventaServiceImpl.create(venta);
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Void>  delete(@PathVariable Integer id) {
+		ventaManager.delete(id);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
+	
 	
 }
---
+++ src/main/java/ar/edu/um/ingenieria/manager/VentaManager.java
@@ -0,0 +1,27 @@
+package ar.edu.um.ingenieria.manager;
+
+import org.springframework.stereotype.Service;
+
+import ar.edu.um.ingenieria.service.impl.VentaServiceImpl;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VentaManager {
+	
+private static final Logger logger = LoggerFactory.getLogger(TemaManager.class);
+	
+	@Autowired
+	private VentaServiceImpl ventaServiceImpl;
+
+	public void delete(Integer id) {
+		ventaServiceImpl.remove(ventaServiceImpl.findById(id));
+		
+	}
+
+}
